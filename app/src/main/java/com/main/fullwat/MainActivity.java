d4657e8c260843610a65ce081dfe0f9c78dfb9f1
44jon44 <jonmadi21@gmail.com>
2022-10-03 14:54:54 +0200

    primer commit

---
+++ app/src/main/java/com/main/fullwat/MainActivity.java
@@ -0,0 +1,68 @@
+package com.main.fullwat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner =  findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+    // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+    // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        pos=parent.getSelectedItemPosition();
+
+        if (pos==1) {
+            Toast.makeText(this, "1", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this,Tiras.class);
+            intent.putExtra("idioma",1);
+            startActivity(intent);
+        }
+        if (pos==2) {
+            Toast.makeText(this, "2", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this,Tiras.class);
+            intent.putExtra("idioma",2);
+            startActivity(intent);
+        }
+        if (pos==3) {
+            Toast.makeText(this, "3", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this,Tiras.class);
+            intent.putExtra("idioma",3);
+            startActivity(intent);
+        }
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/main/fullwat/Tiras.java
@@ -0,0 +1,16 @@
+package com.main.fullwat;
+
+import android.os.Bundle;
+import android.widget.AdapterView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tiras extends AppCompatActivity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tiraslayout);
+    }
+}
