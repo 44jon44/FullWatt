5d937ba5da80b1188854cc2d4528e273cf477268
jonma <jonmadi21@gmail.com>
2022-11-22 13:26:03 +0100

    avance en el desarrollo. Se realiza la petición correctamente pero el método onClickListener ya funciona correctamente

--- app/src/main/java/app/Tiras.java
+++ app/src/main/java/app/Tiras.java
@@ -4,6 +4,7 @@ import static java.lang.Thread.sleep;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,125 +18,65 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.main.fullwat.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class Tiras extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    RequestQueue requestQueue;
+public class Tiras extends AppCompatActivity  {
 
+    ArrayList<String> models = new ArrayList<>();
+    ArrayList<String> ips = new ArrayList<>();
+    ArrayAdapter<String> modelAdapter;
+    ArrayAdapter<String> ipAdapter;
+    RequestQueue requestQueueM;
+    RequestQueue requestQueueIP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int m = 0;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tiraslayout);
+        requestQueueM = Volley.newRequestQueue(this);
+        requestQueueIP = Volley.newRequestQueue(this);
+        String URLModelos = "http://192.168.224.86/fullwat_app_db/fetchModels.php";
+        String URLTensions = "http://192.168.224.86/fullwat_app_db/fetchTensions.php";
+        String URLPotencias = "http://192.168.224.86/fullwat_app_db/fetchPotencias.php";
+        String URLLumens = "http://192.168.224.86/fullwat_app_db/fetchLumens.php";
+        String URLLeds = "http://192.168.224.86/fullwat_app_db/fetchLeds.php";
+        String URLIPs = "http://192.168.224.86/fullwat_app_db/fetchIPs.php";
 
-        Spinner spinnerT = findViewById(R.id.tension);
-        spinnerT.setOnItemSelectedListener(this);
-
-        Spinner spinnerTe = findViewById(R.id.temperatura);
-        spinnerTe.setOnItemSelectedListener(this);
-
-        Spinner spinnerP = findViewById(R.id.potenciaMetro);
-        spinnerP.setOnItemSelectedListener(this);
-
-        Spinner spinnerLu = findViewById(R.id.lumenesMetro);
-        spinnerLu.setOnItemSelectedListener(this);
-
-        Spinner spinnerLe = findViewById(R.id.ledsMetro);
-        spinnerLe.setOnItemSelectedListener(this);
-
-        Spinner spinnerIp = findViewById(R.id.ip);
-        spinnerIp.setOnItemSelectedListener(this);
-
-
-        requestQueue = Volley.newRequestQueue(this);
-        ArrayList<String> models = new ArrayList<>();
-        models = readModel();
-
-
-        try {
-            sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
-                android.R.layout.simple_spinner_item, models);
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinnerM = findViewById(R.id.modelo);
-        spinnerM.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long arg) {
-                int id = parent.getId();
-                System.out.println("Pajin1");
 
-                switch (id) {
-                    case R.id.modelo:
-                        System.out.println("pajin");
-                        break;
-                    case R.id.temperatura:
-                        break;
-                    default:
-                        System.out.println("PAJIN");
-                        break;
-                }
-            }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URLModelos, null, new Response.Listener<JSONObject>() {
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        spinnerM.setAdapter(spinnerArrayAdapter);
-        spinnerArrayAdapter.notifyDataSetChanged();
-
-/*
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerM.setOnItemSelectedListener(this);
-        spinnerM.setAdapter(adapter);
-*/
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
-
-    private ArrayList readModel() {
-        String URL = "http://192.168.224.86/fullwat_app_db/fetchModel.php";
-        ArrayList<String> respuesta = new ArrayList<String>();
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, URL, new JSONArray(), new Response.Listener<JSONArray>() {
-
-            @Override
-            public void onResponse(JSONArray response) {
+            public void onResponse(JSONObject response) {
 
                 System.out.println(response.length());
                 System.out.println(response.toString());
-                for (int i = 0; i < 1; i++) {
-                    try {
-                        System.out.println(response.getJSONObject(i).get("Value"));
-                        respuesta.add(response.getJSONObject(i).get("Value").toString());
 
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                try {
+                    JSONArray jsonArray = response.getJSONArray("models");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject=jsonArray.getJSONObject(i);
+                        String id=jsonObject.optString("ID");
+                        String attribute_id=jsonObject.optString("Attribute_ID");
+                        String filter=jsonObject.optString("Filter");
+                        String tempCol=filter;
+                        String value=jsonObject.optString("Value");
+                        models.add(filter);
+                        modelAdapter = new ArrayAdapter<>(Tiras.this, android.R.layout.simple_spinner_item, models);
+                        modelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinnerM.setAdapter(modelAdapter);
+                        spinnerM.setPrompt("pajinprmt");
                     }
-
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
 
             }
@@ -145,10 +86,86 @@ public class Tiras extends AppCompatActivity implements AdapterView.OnItemSelect
                 Log.d("error", error.toString());
             }
         });
-        requestQueue.add(request);
+        requestQueueM.add(request);
+        spinnerM.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                System.out.println("Pajin");
+            }
 
-        return respuesta;
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        Spinner spinnerT = findViewById(R.id.tension);
+
+
+        Spinner spinnerTe = findViewById(R.id.temperatura);
+
+
+        Spinner spinnerP = findViewById(R.id.potenciaMetro);
+
+
+        Spinner spinnerLu = findViewById(R.id.lumenesMetro);
+
+
+        Spinner spinnerLe = findViewById(R.id.ledsMetro);
+
+
+        Spinner spinnerIp = findViewById(R.id.ip);
+        JsonObjectRequest requestIP = new JsonObjectRequest(Request.Method.GET, URLIPs, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+                System.out.println(response.length());
+                System.out.println(response.toString());
+
+                try {
+                    JSONArray jsonArray = response.getJSONArray("ips");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject=jsonArray.getJSONObject(i);
+                        //String id=jsonObject.optString("ID");
+                        //String attribute_id=jsonObject.optString("Attribute_ID");
+                        //String filter=jsonObject.optString("Filter");
+                        String value3=jsonObject.getString("Value");
+                        ips.add(value3);
+                        ipAdapter = new ArrayAdapter<>(Tiras.this, android.R.layout.simple_spinner_item, ips);
+                        ipAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                        spinnerIp.setAdapter(ipAdapter);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error", error.toString());
+            }
+        });
+        requestQueueIP.add(requestIP);
+        spinnerIp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                System.out.println("pajinIP");
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
     }
+
 
 
 }
